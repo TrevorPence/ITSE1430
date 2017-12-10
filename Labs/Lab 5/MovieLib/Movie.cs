0812a00ffd3d514ee0b73612b017433e941bf3e3
Trevor Pence <31493003+TrevorPence@users.noreply.github.com>
2017-12-09 23:04:32 -0600

    Add files via upload
    Lab 5

---
+++ Labs/Lab 5/MovieLib/Movie.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Trevor Pence
+ * ITSE-1430
+ * 10/9/2017
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieLib
+{
+    public class Movie : IValidatableObject
+    {
+        public string Title
+        {
+            set { _title = value; }
+            get { return _title ?? ""; }
+        }
+        public string Description
+        {
+            set { _description = value; }
+            get { return _description ?? ""; }
+        }
+        public decimal Length { get; set; }
+        public bool IsOwned { get; set; }
+        public int Id { get; set; }
+
+        /// <summary> Checks if the movie is valid </summary>
+        /// <param name="validationContext">  </param>
+        /// <returns> A list of errors in the movie </returns>
+        public IEnumerable<ValidationResult> Validate( ValidationContext validationContext )
+        {
+            if (String.IsNullOrEmpty(_title))
+                yield return new ValidationResult("title cannot be null or empty.", new[] { nameof(Title) });
+
+            if (Length < 0)
+                yield return new ValidationResult("Length cannot be less than 0", new[] { nameof(Length) });
+        }
+
+        private string _title;
+        private string _description;
+    }
+}
